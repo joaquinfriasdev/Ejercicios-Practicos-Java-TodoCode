41baabc53659a7306284e625aa9507787fbac300
Joaquín Frías <joaquin124507@gmail.com>
2023-08-07 12:32:16 -0300

    Update Principal.java

--- AgendaElectronica/src/agendaelectronica/igu/Principal.java
+++ AgendaElectronica/src/agendaelectronica/igu/Principal.java
@@ -15,7 +15,6 @@ public class Principal extends javax.swing.JFrame {
         initComponents();
     }
 
-
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
